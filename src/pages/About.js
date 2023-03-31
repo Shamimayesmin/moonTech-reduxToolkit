a2796c594cf2f157d3637b526fca79ac49f15234
Shamimayesmin <shamimayesmin144@gmail.com>
2023-03-31 12:21:10 +0600

    new add

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
       <h1>This is about page</h1>
       <h1>This is about page</h1>
       <h1>This is about page</h1>
-      <h1>This is about page</h1>
+     
       
     </div>
   );
