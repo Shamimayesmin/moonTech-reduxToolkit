42262ea099b7e2401f9db8534dcf9e38497d5216
Shamimayesmin <shamimayesmin144@gmail.com>
2023-03-31 12:23:23 +0600

    text remove

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -9,8 +9,7 @@ const TopRated = () => {
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
+      
       
     </div>
   );
