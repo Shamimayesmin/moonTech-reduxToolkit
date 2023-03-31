ce4dc93aef78e01e0882f9451fd03cfb248e93fb
Shamimayesmin <shamimayesmin144@gmail.com>
2023-03-31 12:27:37 +0600

    products

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,8 @@ const About = () => {
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is about page</h1>
       <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
       
      
-      
     </div>
   );
 };
