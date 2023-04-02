6f42b170efb3b5beb4672c60f18a8a9b4696d57c
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-02 14:01:45 +0600

    nine

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,9 +5,6 @@ const About = () => {
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       <h1>This is about page</h1>
       <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
-      <h1>This is about page</h1>
       
      
     </div>
