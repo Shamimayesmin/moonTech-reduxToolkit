1372bd701da742888b2b5ebcf2fa3eefcb9757ce
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-03 23:46:38 +0600

    four

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -9,9 +9,6 @@ const TopRated = () => {
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
-      <h1>This is featured page</h1>
       
       
     </div>
