7dc40e2141d00d0acdc19cdad2dc47a682bea8c3
Shamimayesmin <shamimayesmin144@gmail.com>
2023-04-07 23:27:49 +0600

    one

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -6,6 +6,23 @@ const TopRated = () => {
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
       <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
+      <h1>This is featured page</h1>
       
     </div>
   );
